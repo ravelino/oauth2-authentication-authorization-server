56a24b1189f5f2f2e39f58dcfbfefd3341da7453
Rogério Avelino da Silva <avelino_rogerio@yahoo.com.br>
2021-07-05 14:07:04 -0300

    Update LoginPciTokenEnhancer.java

--- oauth2-jwt-server/src/main/java/com/ravelino/oauth/jwt/config/security/LoginPciTokenEnhancer.java
+++ oauth2-jwt-server/src/main/java/com/ravelino/oauth/jwt/config/security/LoginPciTokenEnhancer.java
@@ -13,7 +13,7 @@ public class LoginPciTokenEnhancer implements TokenEnhancer {
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		Map<String, Object> additionalInfo = new HashMap<>();
-		additionalInfo.put("meu_ovo", "esquerdo");
+		additionalInfo.put("aditional-info", "teste info");
 		((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
 		return accessToken;
 	}
